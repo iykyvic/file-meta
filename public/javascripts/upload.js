0034b5abd346f310294614bc95a6e05e84334725
andela-venogwe <victor.enogwe@andela.com>
2017-03-06 11:35:52 +0100

    fix xml async browser issues

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,3 +1,13 @@
+const display = (fileSizes) => {
+  for (let file in fileSizes) {
+    table.innerHTML += `
+      <tr>
+        <th>${file}</th>
+        <th>${fileSizes[file].size}</th>
+      </tr>`;
+  }
+};
+
 const upload = () => {
   const files = document.getElementById('files');
   const error = document.getElementById('error');
@@ -16,17 +26,14 @@ const upload = () => {
     request.open("POST", '/upload', true);
     request.setRequestHeader("content-type", "application/json");
 
+    request.onerror = function (e) {
+      error.innerHTML = request.statusText;
+    };
+
     request.onreadystatechange = () => {
       if(request.readyState == 4 && request.status == 200) {
         const fileSizes = JSON.parse(request.responseText).data;
-        for (let file in fileSizes) {
-          console.log(file)
-          table.innerHTML += `
-            <tr>
-              <th>${file}</th>
-              <th>${fileSizes[file].size}</th>
-            </tr>`;
-        }
+        display(fileSizes);
         table.style.visibility = "visible";
         files.value = '';
       }
